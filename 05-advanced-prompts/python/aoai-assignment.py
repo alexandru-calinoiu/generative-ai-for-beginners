5224b642ebdf565765e9b5c29b199e80e15c5354
Calin <calinoiu.alexandru@agilefreaks.com>
2024-04-13 13:31:39 +0300

    Improve code

--- 05-advanced-prompts/python/aoai-assignment.py
+++ 05-advanced-prompts/python/aoai-assignment.py
@@ -2,9 +2,9 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello():
-    name = request.args.get('name', 'World')
+    name = request.args.get('name', 'Guest')
     return f'Hello, {name}!'
 
 if __name__ == '__main__':
